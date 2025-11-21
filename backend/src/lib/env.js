0bbd5e15ce5aec86468071daddfb05bea6044d9c
Abhinav <2k23.cs2313871@gmail.com>
2025-11-21 16:41:01 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,6 +5,7 @@ dotenv.config();
 export const ENV = {
     PORT: process.env.PORT || 5000,
     DB_URL: process.env.DB_URL || '',
+    NODE_ENV: process.env.NODE_ENV,
 };
 
 export default ENV;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,41 @@
 import express from 'express';
-import {ENV} from "./lib/env.js"
+import {ENV} from "./lib/env.js";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 
 
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.get('/', (req, res) => {
    res.status(200).json({ msg: 'successs from backend hello' });
 });
 
 
-app.listen(ENV.PORT, () => {
-   console.log(`server is listening on port ..`, ENV.PORT);
+const PORT = ENV.PORT || 5000;
+
+app.listen(PORT, () => {
+   console.log(`server is listening on port ${PORT}...`);
 });
 
+
+// how can we distinguish between we are in production or in development
+// making our application ready for deployment
+const MODE = ENV.MODE_ENV || process.env.NODE_ENV || 'development';
+if (MODE === 'production') {
+   // path from backend/src -> ../../frontend/vite-project/dist
+   const staticPath = path.join(__dirname, '../../frontend/vite-project/dist');
+   app.use(express.static(staticPath));
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.join(staticPath, 'index.html'));
+   });
+}
+
+
+app.listen(ENV.PORT,()=> console.log( "Server is running on port:", ENV.PORT));
 export default app;
